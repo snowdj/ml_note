997b3d5a273fae27fd16ceb761f84ebcd9716950
jonduan <jonduan@uvic.ca>
2017-10-21 17:54:28 +0000

    add finance

---
+++ finance/util.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbols from CSV files."""
+    df_final = pd.DataFrame(index=dates)
+    if "SPY" not in symbols:  # add SPY for reference, if absent
+        symbols.insert(0, "SPY")
+
+    for symbol in symbols:
+        file_path = symbol_to_path(symbol)
+        df_temp = pd.read_csv(file_path, parse_dates=True, index_col="Date",
+            usecols=["Date", "Adj Close"], na_values=["nan"])
+        df_temp = df_temp.rename(columns={"Adj Close": symbol})
+        df_final = df_final.join(df_temp)
+        if symbol == "SPY":  # drop dates SPY did not trade
+            df_final = df_final.dropna(subset=["SPY"])
+
+    return df_final
+
+
+def plot_data(df_data, title="Stock Data", ylabel = "Price"):
+    """Plot stock data with appropriate axis labels."""
+    ax = df_data.plot(title=title, fontsize=2)
+    ax.set_xlabel("Date")
+    ax.set_ylabel(ylabel)
+    plt.show() 
